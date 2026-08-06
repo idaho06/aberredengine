b1e822be80931b9b1bd246e78c9305e5083d045b
César Idaho <cesar.idaho@gmail.com>
2026-08-06 23:29:49 +0200

    Sync Windows rc file with Cargo.toml versioning

--- crates/aberredengine/build.rs
+++ crates/aberredengine/build.rs
@@ -2,7 +2,15 @@
 
 #[cfg(windows)]
 fn main() {
-    let _ = embed_resource::compile("aberred.rc", embed_resource::NONE);
+    let macros = [
+        format!("VERSION_MAJOR={}", env!("CARGO_PKG_VERSION_MAJOR")),
+        format!("VERSION_MINOR={}", env!("CARGO_PKG_VERSION_MINOR")),
+        format!("VERSION_PATCH={}", env!("CARGO_PKG_VERSION_PATCH")),
+        format!("VERSION_STR=\"{}\"", env!("CARGO_PKG_VERSION")),
+    ];
+    embed_resource::compile("aberred.rc", &macros)
+        .manifest_optional()
+        .unwrap();
 }
 
 #[cfg(unix)]
